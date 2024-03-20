391520fcd62793c34bf5e5c67ff373ba0cdb2133
Pravin Patekar <125630602+PravinSimbaQuartz@users.noreply.github.com>
2024-03-20 12:52:20 +0530

    updated

--- controller/userController.js
+++ controller/userController.js
@@ -18,15 +18,15 @@ const createUser = async function (req, res) {
         title } = req.body
 
     if (!email) {
-        return res.send({ message: "Email is required" })
+        return res.status(400).send({ message: "Email is required" })
     }
     if (!mobileNumber) {
-        return res.send({ message: "mobileNumber is required" })
+        return res.status(400).send({ message: "mobileNumber is required" })
     }
 
     const checkEmail = await userModel.findOne({ email: email })
     if (checkEmail) {
-        return res.send({ message: "Email already use please try another email" })
+        return res.status(409).send({ message: "Email already use please try another email" })
     }
 
 
@@ -54,13 +54,22 @@ const getUser = async (req, res) => {
 
 
 const getSingleUser = async (req, res) => {
-    const id = req.params.id
+    try {
+        const id = req.params.id
 
-    console.log(3333333, id)
-    // const userData = await userModel.findOne({ email: "abc@gmail.com" })
-    // const userData = await userModel.findOne({ _id: "65e570a4150a086d6c0d21fa" })
-    const userData = await userModel.findById({ _id: id })
-    res.send({ message: "User fetch successfully", userData })
+        // const userData = await userModel.findOne({ email: "abc@gmail.com" })
+        // const userData = await userModel.findOne({ _id: "65e570a4150a086d6c0d21fa" })
+        const userData = await userModel.findById({ _id: id })
+        if (!userData) {
+            return res.status(404).send({
+                message: "User not found"
+            })
+        }
+
+        res.status(200).send({ message: "User fetch successfully", userData })
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
 }
 
 // 1. update ? 
